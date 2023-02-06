0964d97eb532f0f2f4056e0ef9eade4e6888aa8e
uizyk <samkim@sanion.com>
2023-02-06 17:42:26 +0900

    add code

---
+++ MusicListingApp/Models/Artist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicListingApp.Models
+{
+    public class Artist
+    {
+        public int ArtistId { get; set; }
+
+        public string ArtistName { get; set; }
+    }
+}
